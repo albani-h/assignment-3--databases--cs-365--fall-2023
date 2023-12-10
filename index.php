b4e8c35486138e7cd6b04c7a3ef9fdda1ae6fc95
Albani Hunt <alhunt@hartford.edu>
2023-12-10 15:12:32 -0500

    update working, delete working, search working

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Password Database Assignment 3</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <h1>Password Database Assignment 3 </h1>
+</header>
+<form id= "refresh" method="post"
+action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<input id=“Refresh_submit-button" type="submit" value=“Refresh>
+</form>
+
+
+<?php
+require_once "includes/config.php";
+require_once "includes/helpers.php";
+$newtestarray= array("test"=>45, "test2"=>95);
+echo $newtestarray["test"];
+
+$option = (isset($_POST['submitted']) ? $_POST['submitted'] : null);
+if ($option != null) {
+    switch ($option) {
+        case 1:
+            if ("" == $_POST['search']) {
+                echo '<div id="error">Search query empty. Please try again.</div>' .
+                    "\n";
+            } else {
+                if (0 === (search($_POST['search']))) {
+                    echo '<div id="error">Nothing found.</div>' . "\n";
+                }
+            }
+
+            break;
+
+    case 2:
+
+            if (("" == $_POST['website_url']) || ("" == $_POST['website_name'])) {
+                echo '<div id="error">At least one field in your insert request ' .
+                    'is empty. Please try again.</div>' . "\n";
+            } else {
+                insert($_POST['website_url'],$_POST['website_name'],$_POST['username'],$_POST['user_password'],$_POST['account_comment']);
+            }
+
+            break;
+
+        case 3:
+            if (("" == $_POST['current_attribute']) || ("" == $_POST['match'])) {
+                echo '<div id="error">At least one field in your delete procedure ' .
+                    'is empty. Please try again.</div>' . "\n";
+
+            } else {
+                delete($_POST['current_attribute'], $_POST['match']);
+            }
+
+            break;
+
+
+        case 4:
+            if ((0 == $_POST['new-attribute']) && ("" == $_POST['match'])) {
+                echo '<div id="error">One or both fields were empty, ' .
+                    'but both must be filled out. Please try again.</div>' . "\n";
+            } else {
+                update($_POST['current-attribute'], $_POST['new-attribute'],
+                    $_POST['query-attribute'], $_POST['match']);
+            }
+
+            break;
+
+
+    }
+}
+
+?>
+
+
+
+
+
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <fieldset>
+        <legend>Search</legend>
+        <input type="text" name="search" autofocus required>
+        <input type="hidden" name="submitted" value="1">
+        <p><input type="submit" value="search"></p>
+    </fieldset>
+</form>
+
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <fieldset>
+        <legend>Insert</legend>
+        INSERT INTO ACCOUNT VALUES ( <input type="text" name="website_url" placeholder="WEBSITE URL" required>, <input type="text" name="website_name" placeholder="WEBSITE NAME" required>, <input type="text" name="username" placeholder="USERNAME" required>,<input type="text" name="user_password" placeholder="PASSWORD" required>,<input type="text" name="account_comment" placeholder="COMMENT" required>);
+        <input type="hidden" name="submitted" value="2">
+        <p><input type="submit" value="insert"></p>
+    </fieldset>
+</form>
+
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <fieldset>
+        <legend>Delete</legend>
+        DELETE ACCOUNT WHERE
+        <select name="current_attribute" id="current_attribute">
+            <option>Username</option>
+            <option>Password</option>
+            <option>Email</option>
+        </select>
+        = <input type="text" name="match" required>
+        <input type="hidden" name="submitted" value="3">
+        <p><input type="submit" value="delete"></p>
+    </fieldset>
+</form>
+</body>
+</html>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <fieldset>
+        <legend>Update</legend>
+        UPDATE Account
+        <select name="current-attribute" id="current-attribute">
+            <option>email</option>
+            <option>username</option>
+            <option>user_password</option>
+        </select>
+        = <input type="text" name="new-attribute" required> WHERE
+        <select name="query-attribute" id="query-attribute">
+            <option>email</option>
+            <option>username</option>
+            <option>user_password</option>
+        </select>
+        = <input type="text" name="match" required>
+        <input type="hidden" name="submitted" value="4">
+        <p><input type="submit" value="update"></p>
+    </fieldset>
+</form>
--- mysql/setup.sql
+++ mysql/setup.sql
@@ -4,3 +4,43 @@ DROP USER IF EXISTS 'passwords_user'@'localhost';
 CREATE USER 'passwords_user'@'localhost'IDENTIFIED BY '123';
 GRANT ALL ON student_passwords. * TO 'passwords_user'@'localhost';
 USE student_passwords;
+CREATE TABLE user_info(
+                        user_id INT NOT NULL AUTO_INCREMENT,
+                        email varchar(75),
+                        PRIMARY KEY (user_id)
+
+);
+
+CREATE TABLE account_entry(
+                            user_ip INT NOT NULL AUTO_INCREMENT,
+                            website_url VARCHAR(100),
+                            website_name VARCHAR (100) NOT NULL,
+                            username varchar(15)NOT NULL,
+                            user_password VARBINARY(30),
+                            account_comment VARCHAR (250)NOT NULL,
+                            PRIMARY KEY (user_ip,username, website_name)
+);
+
+INSERT INTO user_info (email) VALUES ( "cset@yahoo.com");
+INSERT INTO user_info (email) VALUES ( "emgil@yahoo.com");
+INSERT INTO user_info (email) VALUES ("rchad@yahoo.com");
+INSERT INTO user_info (email) VALUES ("arusso@outlook.com");
+INSERT INTO user_info (email) VALUES ( "jrusso@yahoo.com");
+INSERT INTO user_info (email) VALUES ("mrusso@yahoo.com");
+INSERT INTO user_info (email) VALUES ("hmon@gmail.com");
+INSERT INTO user_info (email) VALUES ("rbax@bing.com");
+INSERT INTO user_info (email) VALUES ("cwilde@yahoo.com");
+INSERT INTO user_info (email) VALUES ("idaniels@yahoo.com");
+
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ( "https://www.disneyplus.com","Disney+", "cset",AES_ENCRYPT("red","key"),"password created for Cleo Setori" );
+INSERT INTO account_entry(website_url,website_name,username,user_password,account_comment)  VALUES ( "http://www.sephora.com","Sephora.com","emgil", AES_ENCRYPT("orange","key"),"password created for emgil");
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ( "https://www.youtube","YouTube", "rchad",AES_ENCRYPT("yellow","key"), "password created for username rchad");
+INSERT INTO account_entry(website_url,website_name,username,user_password,account_comment)  VALUES ( "http://max.com","max.com ","arusso", AES_ENCRYPT("green","key"), "password created for hbomax user: arusso");
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ( "http://www.wordpress.com","WordPress.com","jrusso", AES_ENCRYPT("blue","key"),"password created for WordPress user :jrusso" );
+INSERT INTO account_entry(website_url,website_name,username,user_password,account_comment)  VALUES ( "http://www.amazon.com","amazon.com","mrusso", AES_ENCRYPT("indigo","key"),"new amazon account registered");
+INSERT INTO account_entry(website_url,website_name,username,user_password,account_comment)  VALUES ( "http://www.paramountplus.com","Paramount Plus","hannahmontana", AES_ENCRYPT("violet","key"), "password created for Paramount Plus user Max Russo");
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ("http://www.bhphotovideo.com","B&H","rbaxter", AES_ENCRYPT("sage","key"),"new account for Raven Baxter");
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ("http://www.apple.com","Apple.com","cwilde", AES_ENCRYPT("lavender777","key"), "Apple password for user: cwilde created.");
+INSERT INTO account_entry (website_url,website_name,username,user_password,account_comment) VALUES ("http://www.soundcloud.com","SoundCloud.com","idaniels", AES_ENCRYPT("dogs1234","key"),"Soundcloud user idaniels has created password.");
+
+CREATE VIEW EVERYTHING AS SELECT * FROM USER_INFO,ACCOUNT_ENTRY WHERE user_info.user_id=account_entry.user_ip;
